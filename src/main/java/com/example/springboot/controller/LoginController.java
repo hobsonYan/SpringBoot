6079d4f8bdbac556444e4f44e15ff3dc9e405d43
yan hongbo <yanhb21@chinaunicom.cn>
2019-11-13 10:33:13 +0800

    add interface to change password, add interface to get forgotten password by email or phone

--- src/main/java/com/example/springboot/controller/LoginController.java
+++ src/main/java/com/example/springboot/controller/LoginController.java
@@ -2,10 +2,7 @@ package com.example.springboot.controller;
 
 import com.example.springboot.common.Result;
 import com.example.springboot.service.LoginService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +22,7 @@ public class LoginController {
     @Resource
     private LoginService loginService;
 
-    @ApiModelProperty(value = "用户登录", notes = "用户登录")
+    @ApiOperation(value = "用户登录", notes = "用户登录")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "username", value = "用户名", required = true),
             @ApiImplicitParam(name = "password", value = "密码", required = true)
@@ -35,7 +32,7 @@ public class LoginController {
         return loginService.userLogin(username, password);
     }
 
-    @ApiModelProperty(value = "修改密码", notes = "修改密码")
+    @ApiOperation(value = "修改密码", notes = "修改密码")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "prePassword", value = "旧密码", required = true),
             @ApiImplicitParam(name = "newPassword", value = "新密码", required = true)
@@ -45,6 +42,23 @@ public class LoginController {
         return loginService.updatePassword(prePassword, newPassword);
     }
 
+    @ApiOperation(value = "忘记密码，通过手机号进行修改", notes = "忘记密码，通过手机号进行修改")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", required = true),
+            @ApiImplicitParam(name = "phone", value = "手机号", required = true)
+    })
+    @PostMapping("/phone/forgetPassword")
+    public Result forgetPasswordByPhone(String username, String phone) {
+        return loginService.forgetPasswordByPhone(username, phone);
+    }
 
-
+    @ApiOperation(value = "忘记密码，通过邮箱进行修改", notes = "忘记密码，通过邮箱进行修改")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", required = true),
+            @ApiImplicitParam(name = "email", value = "邮箱", required = true)
+    })
+    @PostMapping("/email/forgetPassword")
+    public Result forgetPasswordByEmail(String username, String email) {
+        return loginService.forgetPasswordByEmail(username, email);
+    }
 }
